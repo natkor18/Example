84e6e82360772caddc1539d05e4f9e1e60ee5346
natalia18 <natkor18@gmail.com>
2024-12-03 19:34:10 +0300

    add mull

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ def plus(a, b):
 def minus(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
 
 if __name__ == '__main__':
     a, b = 123, 256
     print(plus(a, b))
     print(minus(a, b))
+    print(mul(a, b))
