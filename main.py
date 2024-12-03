772e22d039290022bf7c51b6f4efd66ce1d08dfd
natalia18 <natkor18@gmail.com>
2024-12-03 19:44:39 +0300

    add message print

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def mul(a, b):
 def div(a, b):
     return a // b
 
+def mess(text):
+    return text * 3
+
 
 if __name__ == '__main__':
     a, b = 123, 256
@@ -17,3 +20,5 @@ if __name__ == '__main__':
     print(minus(a, b))
     print(mul(a, b))
     print(div(a, b))
+    text = 'Hello! '
+    print(mess(text))
